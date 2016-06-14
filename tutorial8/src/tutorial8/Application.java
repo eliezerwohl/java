4e8207c489be560faf42c9dfe3fad858a0c0ae7b
eliezerwohl <eliezerwohl@gmail.com>
2016-06-14 18:23:20 -0400

    made while loop

---
+++ tutorial8/src/tutorial8/Application.java
@@ -0,0 +1,27 @@
+package tutorial8;
+
+import java.util.Scanner;
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		int value = 0;
+		do{
+			System.out.println("Enter a number fool!: ");
+			value = scanner.nextInt();	
+		}
+		while(value !=5);
+		
+		/*
+		System.out.println("Enter a number fool!: ");
+		int value = scanner.nextInt();
+		
+		while (value !=5 ){
+			System.out.println("Enter a different number!: ");
+			value = scanner.nextInt();
+		}
+		*/
+		System.out.println("You got it");
+	}
+
+}
